6f1db9150c75411a10356ec7bb0c29dd92c2a4cc
Joe Guida <joeguida@outlook.com>
2021-11-19 10:07:15 -0500

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,28 @@
-#include "vector.h"
-#include <iostream>
+#include "scene.h"
+#include "sphere.h"
 
 int main() {
 
-	// Image Dimensions
-	const int image_height = 256;
-	const int image_width = 256;
+	// Create the scene
+	const Vector2 windowSize(256, 256);
+	const Camera cammera(Vector3(0, 0, -1000), Normalized(Vector3(0, 0, 1)));
+	Scene scene(windowSize, cammera);
+	
+	// Create some objects
+	Sphere spheres[5];
+	spheres[0] = Sphere(Vector3(0, 0, 0), 1);
+	spheres[1] = Sphere(Vector3(0, 0, 1), 1);
+	spheres[2] = Sphere(Vector3(-1, 0, 0), 1);
+	spheres[3] = Sphere(Vector3(1, 0, 0), 1);
+	spheres[4] = Sphere(Vector3(1, 0, 1), 1);
+	
+	// Add the objects to the scene
+	for (Sphere& sphere : spheres) 
+		scene.Add(sphere);
+	
+	// Raytrace the scene
+	scene.Raytrace();
 
-	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-	for (int i = 0; i < image_height; i++) {
-		for (int j = 0; j < image_width; j++) {
-
-			//std::cout << r << ' ' << g << ' ' << b << std::endl;
-		}
-	}
 
 	return 0;
 }
