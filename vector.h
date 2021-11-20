dcd5fcaa42b215eacd864e9d8fa7d84a8c0dd0af
Joe Guida <joeguida@outlook.com>
2021-11-19 22:57:51 -0500

    Move functions outside struct, remove assignment functions, remove class methods for normalize and magnitude

--- vector.h
+++ vector.h
@@ -16,28 +16,7 @@ typedef struct Vector2 {
 		y = b;
 	}
 
-	inline Vector2 operator +(const Vector2& v) const;
-	inline Vector2 operator +(float s) const;
-	inline Vector2 operator -(const Vector2& v) const;
-	inline Vector2 operator -(float s) const;
-	inline Vector2 operator *(const Vector2& v) const;
-	inline Vector2 operator *(float s) const;
-	inline Vector2 operator /(const Vector2& v) const;
-	inline Vector2 operator /(float s) const;
-
-	inline void operator +=(const Vector2& v);
-	inline void operator +=(float s);
-	inline void operator -=(const Vector2& v);
-	inline void operator -=(float s);
-	inline void operator *=(const Vector2& v);
-	inline void operator *=(float s);
-	inline void operator /=(const Vector2& v);
-	inline void operator /=(float s);
-
-	inline void Normalize();
-	inline float Magnitude();
-
-} Vector2; 
+} Vector2;
 
 typedef struct Vector3 {
 
@@ -57,39 +36,37 @@ typedef struct Vector3 {
 		z = c;
 	}
 
-	inline Vector3 operator +(const Vector3& v) const;
-	inline Vector3 operator +(float s) const;
-	inline Vector3 operator -(const Vector3& v) const;
-	inline Vector3 operator -(float s) const;
-	inline Vector3 operator *(const Vector3& v) const;
-	inline Vector3 operator *(float s) const;
-	inline Vector3 operator /(const Vector3& v) const;
-	inline Vector3 operator /(float s) const;
-
-	inline void operator +=(const Vector3& v);
-	inline void operator +=(float s);
-	inline void operator -=(const Vector3& v);
-	inline void operator -=(float s);
-	inline void operator *=(const Vector3& v);
-	inline void operator *=(float s);
-	inline void operator /=(const Vector3& v);
-	inline void operator /=(float s);
-
-	inline void Normalize();
-	inline float Magnitude() const;
-
 } Vector3;
 
-inline Vector2 Normalized(const Vector2& v);
-inline Vector3 Normalized(const Vector3& v);
-inline float Magnitude(const Vector2& v);
-inline float Magnitude(const Vector3& v);
-inline float Dot(const Vector2& a, const Vector2& b);
-inline float Dot(const Vector3& a, const Vector3& b);
-inline Vector3 Cross(const Vector3& a, const Vector3& b);
-inline Vector2 Project(const Vector2& a, const Vector2& b);
-inline Vector3 Project(const Vector3& a, const Vector3& b);
-inline Vector2 Reject(const Vector2& a, const Vector2& b);
-inline Vector3 Reject(const Vector3& a, const Vector3& b);
+Vector2 operator +(const Vector2& a, const Vector2& b);
+Vector2 operator +(const Vector2& v, float s);
+Vector2 operator -(const Vector2& a, const Vector2& b);
+Vector2 operator -(const Vector2& v, float s);
+Vector2 operator *(const Vector2& a, const Vector2& b);
+Vector2 operator *(const Vector2& v, float s);
+Vector2 operator /(const Vector2& a, const Vector2& b);
+Vector2 operator /(const Vector2& v, float s);
+
+Vector2 Normalized(const Vector2& v);
+float Magnitude(const Vector2& v);
+float Dot(const Vector2& a, const Vector2& b);
+Vector2 Project(const Vector2& a, const Vector2& b);
+Vector2 Reject(const Vector2& a, const Vector2& b);
+
+Vector3 operator +(const Vector3& a, const Vector3& b);
+Vector3 operator +(const Vector3& v, float s);
+Vector3 operator -(const Vector3& a, const Vector3& b);
+Vector3 operator -(const Vector3& v, float s);
+Vector3 operator *(const Vector3& a, const Vector3& b);
+Vector3 operator *(const Vector3& v, float s);
+Vector3 operator /(const Vector3& a, const Vector3& b);
+Vector3 operator /(const Vector3& v, float s);
+
+Vector3 Normalized(const Vector3& v);
+float Magnitude(const Vector3& v);
+float Dot(const Vector3& a, const Vector3& b);
+Vector3 Cross(const Vector3& a, const Vector3& b);
+Vector3 Project(const Vector3& a, const Vector3& b);
+Vector3 Reject(const Vector3& a, const Vector3& b);
 
 #endif
