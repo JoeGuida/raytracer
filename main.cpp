f64a50e52d04d868b8a3b2974fd9ba3f2fc5a806
Joe Guida <joeguida@outlook.com>
2021-11-19 22:59:43 -0500

    Update sphere constructors

--- main.cpp
+++ main.cpp
@@ -9,19 +9,20 @@ int main() {
 	Scene scene(windowSize, cammera);
 	
 	// Create some objects
-	Sphere spheres[5];
-	spheres[0] = Sphere(Vector3(0, 0, 0), 1);
-	spheres[1] = Sphere(Vector3(0, 0, 1), 1);
-	spheres[2] = Sphere(Vector3(-1, 0, 0), 1);
-	spheres[3] = Sphere(Vector3(1, 0, 0), 1);
-	spheres[4] = Sphere(Vector3(1, 0, 1), 1);
+	Sphere spheres[6];
+	spheres[0] = Sphere(Vector3(0, 0, 0), 1, Material(Color(255, 0, 0)));
+	spheres[1] = Sphere(Vector3(0, 0, 1), 1, Material(Color(0, 255, 0)));
+	spheres[2] = Sphere(Vector3(-1, 0, 0), 1, Material(Color(0, 0, 255)));
+	spheres[3] = Sphere(Vector3(1, 0, 0), 1, Material(Color(255, 255, 0)));
+	spheres[4] = Sphere(Vector3(1, 0, 1), 1, Material(Color(0, 255, 255)));
+	spheres[5] = Sphere(Vector3(1, 0, 1), 1, Material(Color(255, 0, 255)));
 	
 	// Add the objects to the scene
 	for (Sphere& sphere : spheres) 
 		scene.Add(sphere);
 	
 	// Raytrace the scene
-	scene.Raytrace();
+	scene.RayTrace();
 
 
 	return 0;
