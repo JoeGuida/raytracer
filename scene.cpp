01f39c1c123fe873f8f513d968c142c2f21fce73
Joe Guida <joeguida@outlook.com>
2021-11-23 20:39:34 -0500

    Add lights, ComputeLighting method

--- scene.cpp
+++ scene.cpp
@@ -1,8 +1,10 @@
 #include "scene.h"
+#include <iostream>
 
 Color Scene::TraceRay(const Ray& ray, Hit& hit, float tmin, float tmax) {
 	float closest_t = INFINITY;
 	Sphere* closestSphere = NULL;
+	Color result = Color();
 	for (Sphere& sphere : spheres) {
 		float t1, t2 = 0;
 		sphere.intersects(ray, &t1, &t2);
@@ -20,6 +22,40 @@ Color Scene::TraceRay(const Ray& ray, Hit& hit, float tmin, float tmax) {
 		return Color(255, 255, 255);
 	}
 
+	// Point = RayOrigin + RayDirection * ClosestT
+	// Normal = HitPoint - Sphere Center
 	hit.point = ray.origin + ray.direction * closest_t;
-	return closestSphere->color;
+	hit.normal = Normalized(hit.point - closestSphere->center);
+	result = closestSphere->color;
+	result = result * ComputeLighting(hit.point, hit.normal);
+
+	return Color(int(result.r), int(result.g), int(result.b));
 }
+
+float Scene::ComputeLighting(const Point& point, const Vector3& normal) {
+	float i = 0;
+
+	for (const Light& light : lights) {
+		if (light.type == AMBIENT) {
+			i += light.intensity;
+		}
+		else {
+			Vector3 L;
+			if (light.type == DIRECTIONAL) {
+				L = Normalized(light.direction);
+			}
+			else if (light.type == POINT) {
+				L = Normalized(light.position - point);
+			}
+
+			float nDotl = Dot(normal, L);
+			if (nDotl > 0) {
+				i += light.intensity * nDotl / (Magnitude(normal) * Magnitude(L));
+			}
+		}
+	}
+
+	return std::min(i, 1.0f);
+}
+
+
--- scene.h
+++ scene.h
@@ -5,11 +5,15 @@
 #include "canvas.h"
 #include "viewport.h"
 #include "hit.h"
+#include "light.h"
+#include "point.h"
 #include <vector>
 
 typedef struct Scene {
 
 	std::vector<Sphere> spheres;
+	std::vector<Light> lights;
+
 	Canvas canvas;
 	Viewport viewport;
 
@@ -22,12 +26,17 @@ typedef struct Scene {
 		viewport = v;
 	}
 
-	void Add(Sphere sphere) {
+	void Add(const Sphere& sphere) {
 		spheres.push_back(sphere);
 	}
 
+	void Add(const Light& light) {
+		lights.push_back(light);
+	}
+
 	Color TraceRay(const Ray& ray, Hit& hit, float tmin, float tmax);
-	Color Diffuse(const Vector3& point);
+	float ComputeLighting(const Point& point, const Vector3& normal);
+
 
 } Scene;
 
