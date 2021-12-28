c5717196a95ddc648c2547c148102b6a06feebca
Joe Guida <joeguida@outlook.com>
2021-12-28 02:39:22 -0500

    Add mathhelpers for helper math functions

---
+++ src/headers/mathhelpers.h
@@ -0,0 +1,9 @@
+#ifndef _MATH_HELPERS_H_
+#define _MATH_HELPERS_H_
+
+#include "color.h"
+
+float lerp(float a, float b, float x);
+Color lerp(const Color& a, const Color& b, float x);
+
+#endif
---
+++ src/source/mathhelpers.cpp
@@ -0,0 +1,9 @@
+#include "mathhelpers.h"
+
+float lerp(float a, float b, float x) {
+	return a * (1 - x) + b * x;
+}
+
+Color lerp(const Color& a, const Color& b, float x) {
+	return a * (1 - x) + b * x;
+}
