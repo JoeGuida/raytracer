a17ed52005dcbe92fce5e6156503bc3e64d072f0
Joe Guida <joeguida@outlook.com>
2022-01-22 21:07:22 -0500

    Add degree/radian conversion functions

--- src/headers/mathhelpers.h
+++ src/headers/mathhelpers.h
@@ -8,5 +8,7 @@
 float lerp(float a, float b, float x);
 Color lerp(const Color& a, const Color& b, float x);
 bool cmp_float(float a, float b);
+float radians_to_degrees(float radians);
+float degrees_to_radians(float degrees);
 
 #endif
--- src/source/mathhelpers.cpp
+++ src/source/mathhelpers.cpp
@@ -11,3 +11,11 @@ Color lerp(const Color& a, const Color& b, float x) {
 bool cmp_float(float a, float b) {
 	return (fabsf((a)-(b)) <= FLT_EPSILON * fmaxf(1.0f, fmaxf(fabsf(a), fabsf(b))));
 }
+
+float radians_to_degrees(float radians) {
+	return radians * 57.295754f;
+}
+
+float degrees_to_radians(float degrees) {
+	return degrees * 0.0174533f;
+}
