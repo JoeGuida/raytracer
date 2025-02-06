562c8d5c195ff91f64a8553b02da411198232610
unknown <joeguida@outlook.com>
2025-02-06 02:30:12 -0800

    update image examples

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main() {
 	Scene scene;
 	Scene::load_from_file(FileSystem::get_path("/scenes/test.scene"), scene);
 	Renderer::initialize(scene.width, scene.height, scene.background_color, RenderMode::BLINN_PHONG);
-	std::ofstream file(FileSystem::get_path("/img/blinn_phong.ppm"));
+	std::ofstream file(FileSystem::get_path("/img/image.ppm"));
 	Renderer::raytrace_image(file, scene);
 	return 0;
 }
